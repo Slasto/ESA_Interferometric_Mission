34eda765d8906014242f3d7c4cd363a5c896f7ff
Slasto <32agostino@gmail.com>
2024-12-03 17:02:36 +0100

    file di invio soluzione

---
+++ code/modules/submission_helper.py
@@ -0,0 +1,36 @@
+""" Submission helper to generate a .json-file for submission to optimize.esa.int. """
+import json
+import numpy
+
+def create_submission(challenge_id, problem_id, x, fn_out = './submission.json', name = '', description= ''):
+    """ The following parameters are mandatory to create a submission file:
+
+        challenge_id: a string of the challenge identifier (found on the corresponding problem page)
+        problem_id: a string of the problem identifier (found on the corresponding problem page)
+        x: for single-objective problems: a list of numbers determining the decision vector
+           for multi-objective problems: a list of list of numbers determining a population of decision vectors
+
+        Optionally provide:
+        fn_out: a string indicating the output path and filename
+        name: a string that can be used to give your submission a title
+        description: a string that can contain meta-information about your submission
+    """
+
+    assert isinstance(challenge_id, str)
+    assert isinstance(problem_id, str)
+    assert isinstance(x, (list, numpy.ndarray))
+    assert isinstance(fn_out, str)
+    assert isinstance(name, str)
+    assert isinstance(description, str)
+
+    # converting numpy datatypes to python datatypes
+    x = numpy.array(x).tolist()
+
+    d = {'decisionVector':x,
+         'problem':problem_id,
+         'challenge':challenge_id,
+         'name':name,
+         'description':description }
+
+    with open(fn_out, 'wt') as json_file:
+        json.dump([d], json_file, indent = 6)
