6456910819fe69a7b2a5f1d742fc059c9761f0d8
Slasto <32agostino@gmail.com>
2024-12-10 15:56:22 +0100

    print_result, ora sempre su 2 immagini

--- code/modules/plotter.py
+++ code/modules/plotter.py
@@ -1,7 +1,8 @@
 from modules.golomb_simple import orbital_golomb_array
+from IPython.display import display, Markdown
 
 
-def print_result(udp : orbital_golomb_array, x_solution, N_obs : int = 300, image_path : str = "../data/nebula.jpg") -> None:
+def print_result(udp : orbital_golomb_array, x_solution, N_obs : int = 300) -> None:
     """ Prints the result details and visualizes the given solution.
 
     Args:
@@ -11,9 +12,12 @@ def print_result(udp : orbital_golomb_array, x_solution, N_obs : int = 300, imag
         image_path (`str`, optional): Path to the image used in simulated reconstruction.
     Returns: None
     """
-    fit_0 = udp.fitness(x_solution)[0]
-    fit_1 = fit_0 + 1
-    print("solution: ", x_solution)
-    print("worst Fill factor: {:.5f}".format(fit_0), " ,\t worst Empty factor(?) : {:.5f}".format(fit_1))
+    fit = udp.fitness(x_solution)[0]
+    display(Markdown("---"))
+    print("Solution: ", x_solution)
+    print("Fitness: {:.5f}".format(fit))
     udp.plot(x_solution, figsize=(25,7))
-    udp.plot_simulated_reconstruction(x_solution, N_obs, image_path=image_path)
+    display(Markdown("---"))
+    udp.plot_simulated_reconstruction(x_solution, N_obs, image_path="../data/star.jpg")
+    display(Markdown("---"))
+    udp.plot_simulated_reconstruction(x_solution, N_obs, image_path="../data/nebula.jpg")
