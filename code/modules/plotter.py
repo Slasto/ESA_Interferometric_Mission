6429db8fd1ffd24445d29aad4d7789d63876d7a3
Slasto <32agostino@gmail.com>
2024-12-21 15:54:43 +0100

    ri-runnati i DE, upt print_result

--- code/modules/plotter.py
+++ code/modules/plotter.py
@@ -29,7 +29,7 @@ def print_result(udp : orbital_golomb_array, x_solution, N_obs : int = 300, show
     distance, sat = compute_unique_distances_and_sats_in_grid(udp,x_solution)
     print("N sat: ", udp.n_sat,"\tGrid size: ", udp.grid_size) 
     print("Solution: ", x_solution)
-    print(f"Fitness: {udp.fitness(x_solution)}\tDiverse Distances [%]: {distance:.4f}\tSatellites in Grid [%]: {sat:.4f}")
+    print(f"Default Fitness: {udp.fitness(x_solution)[0]}\tDiverse Distances [%]: {distance:.4f}\tSatellites in Grid [%]: {sat:.4f}")
     udp.plot(x_solution, figsize=(25,7))
 
     if show_simulated_reconstruction:
